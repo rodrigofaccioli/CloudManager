c582b07fa6433239210846dfda6cf1f42626c31a
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-12-26 17:52:06 -0200

    Added to call vs_main.py

--- virtual_screening/vs_run.sh
+++ virtual_screening/vs_run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #All paths must require the termination /  
-#Example: ./vs_run.sh /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/nodefile /home/faccioli/Execute/docking/virtual_screening/ligand/pdbqt/ /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/receptor/pdbqt/ /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/config.ini /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/config_complexo_dm.txt
+#Example: ./vs_run.sh /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/nodefile /home/faccioli/Execute/docking/virtual_screening/ligand/pdbqt/ /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/receptor/pdbqt/ /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/config.ini /home/faccioli/Execute/docking/virtual_screening/3u1i_complexo/config_complexo_dm.txt /disco1/execute/vs_3u1i_complexo_dm/ /root/programs/drugdesign/virtualscreening/
 nodefile=$1
 path_of_pdbqt_ligands=$2
 path_of_pdbqt_receptors=$3
 path_of_config_vs=$4
 path_of_config_vina=$5
-path_execution=$6
+path_execution_node_node=$6
+path_drugdesign=$7
 
 here=$(pwd)
-total_rep=1
-
 
 cd "$path_of_pdbqt_receptors"
 find . -maxdepth 1 -type f -name "*.pdbqt" | sed 's/.\///g' > "$here""/""temporary_list_pdbqt"
@@ -21,8 +20,7 @@ total_pdbqts=$(wc -l "temporary_list_pdbqt" | awk '{print $1}')
 
 total_nodes=$(wc -l "$nodefile" | awk '{print $1}')
 
-
-#creates the execution list
+#creates the execution list for all nodes
 rm "execution_list_"* 2>/dev/null
 node=1
 pdbqt=1
@@ -45,18 +43,19 @@ done
 rm "temporary_list_pdbqt"
 
 
+#send files necessary to run virtual screening to all nodes 
 node=1
 while [ $node -le $total_nodes ]; do
 
 	node_name=$(head -n $node "$nodefile" | tail -n 1)
 	#creating execution directory in node
-	ssh "$node_name" mkdir -p "$path_execution"
+	ssh "$node_name" mkdir -p "$path_execution_node"
 	#coping the ligands to node
-	path_ligand_node="$path_execution""ligand"
+	path_ligand_node="$path_execution_node""ligand"
 	ssh "$node_name" mkdir -p $path_ligand_node
 	scp "$path_of_pdbqt_ligands"* "$node_name":$path_ligand_node
 	#coping the receptors that will be ran in node
-	path_receptor_node="$path_execution""receptor"
+	path_receptor_node="$path_execution_node""receptor"
 	ssh "$node_name" mkdir -p $path_receptor_node
 	total_receptors=$(wc -l "execution_list_""$node_name" | awk '{print $1}')
 	n_node=1
@@ -66,7 +65,7 @@ while [ $node -le $total_nodes ]; do
 		let n_node=$n_node+1	
 	done
 
-#	nohup ssh "$node_name" "cd ""$path_to_pdbs"" ; ./run.sh execution_list_""$node_name"" ""$total_rep" "$gmxpath"  > /dev/null 2>&1&
+	nohup ssh "$node_name" "cd ""$path_execution_node"" ; python ""$path_execution_node""vs_main.py "> /dev/null 2>&1&
 
 	let node=$node+1
 done
