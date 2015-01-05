a92600fe3d2281c1fa6907842ff7e693393a2931
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2015-01-05 10:39:39 -0200

    Updated to coping all files automatically

--- virtual_screening/gather.sh
+++ virtual_screening/gather.sh
@@ -1,25 +1,34 @@
-#!/bin/bash
+#Important: All paths must require termination with / 
+#Example: gather.sh nodefile /disco1/execute/vs_3u1i_complexo_dm/ /disco1/execute/run_vs/
 
 nodefile=$1
-path_to_pdbs=$2
-destination=$3
+source_path=$2
+destination_path=$3
 
 here=$(pwd)
-total_rep=5
-
 
 total_nodes=$(wc -l "$nodefile" | awk '{print $1}')
 
 node=1
 while [ $node -le $total_nodes ]; do
 
-	node_name=$(head -n $node "$nodefile" | tail -n 1)
+        node_name=$(head -n $node "$nodefile" | tail -n 1)
 
-	echo -n "$node_name"" ... "
+        echo -ne "Starting ""$node_name"" ..... \n"
+        #coping log
+        echo "Coping log .... "
+        scp -r $node_name:$source_path"log/" $destination_path > /dev/null
+        echo "OK"
+        #coping structures
+        echo "Coping structures ..... "
+        scp -r $node_name:$source_path"structures/" $destination_path > /dev/null 
+        echo "OK"
+        #coping analysis
+        echo "Coping analysis ....... "
+        scp -r $node_name:$source_path"analysis/" $destination_path > /dev/null 
+        echo "OK"
 
-	scp "$node_name":"$path_to_pdbs""/"* "$destination" >/dev/null 
+        echo -ne "Finished ""$node_name""\n"
 
-	echo "ok"
-
-	let node=$node+1
+        let node=$node+1
 done
